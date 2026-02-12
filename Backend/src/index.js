c8969d7ff1098adf60c18533203320396f8febdb
vishveshshukla0008 <vishveshshukla82@gmail.com>
2026-02-13 01:36:27 +0530

    CORS ERROR

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 if (process.env.NODE_ENV !== "production") {
   app.use(
     cors({
-      origin: "http://localhost:5173",
+      origin: "https://pingme-chatapp-1-kjr4.onrender.com",
       credentials: true,
     })
   );
